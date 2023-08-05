309dc61fda81385f859bb5e677189c99e24824fd
Sendiko <sarangtawon897@gmail.com>
2023-08-05 09:33:39 +0700

    enhancement - create order entity

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "transaction_id",
+        "restaurant_name",
+        "table_number",
+        "total_price",
+        "payed",
+        "menu_ids"
+    ];
+}
---
+++ database/migrations/2023_08_04_142516_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("transaction_id");
+            $table->string("restaurant_name");
+            $table->string("table_number");
+            $table->string("total_price");
+            $table->tinyInteger("payed");
+            $table->json('menu_ids');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
