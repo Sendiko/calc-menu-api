10ef52bf58f83ac91a982ccd3878838c0f426585
Sendiko <sarangtawon897@gmail.com>
2023-08-17 10:23:31 +0700

    enhancement - adjust show and store method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Menu;
 use App\Models\Order;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class OrderController extends Controller
@@ -28,28 +29,30 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = $request->validate([
+        $validated_data = $request->validate([
             "table_number" => "required|string|max:255",
             "menu_ids" => "required|array",
+            "menu_notes" => "array"
         ]);
 
         $emp = auth()->user();
         $restaurant = Restaurant::find($emp->restaurant_id);
-        $menu_ids = json_encode($validator["menu_ids"]); // Convert array to JSON
-    
-        $menu_data = [];
-        $total_price = 0;
-    
-        foreach ($validator["menu_ids"] as $id) {
-            $menu = Menu::find($id);
-            $menu_data[] = [
-                "menu_name" => $menu->name,
-                "menu_price" => $menu->price
+        $menu_data = array_map(function ($menuId, $menuNote) use ($validated_data) {
+            $menu = Menu::find($menuId);
+            return [
+                'menu_name' => $menu->name,
+                'menu_price' => $menu->price,
+                'menu_notes' => $menuNote,
             ];
-            $total_price += $menu->price;
-        }
+        }, $validated_data['menu_ids'], $validated_data['menu_notes']);
 
-        function generateTransactionId() {
+        $total_price = array_reduce($validated_data['menu_ids'], function ($carry, $menuId) {
+            $menu = Menu::find($menuId);
+            return $carry + $menu->price;
+        }, 0);
+
+        function generateTransactionId()
+        {
             $today = date('Ymd');
             $id = $today . Str::random(6);
             return $id;
@@ -57,13 +60,14 @@ class OrderController extends Controller
 
         $order = Order::create([
             "transaction_id" => generateTransactionId(),
-            "table_number" => $validator["table_number"],
+            "table_number" => $validated_data["table_number"],
             "restaurant_name" => $restaurant->name,
             "total_price" => $total_price,
             "payed" => 0,
-            "menu_ids" => $menu_ids 
+            "menu_ids" => json_encode($validated_data["menu_ids"]),
+            "menu_notes" => json_encode($validated_data["menu_notes"])
         ]);
-    
+
         return response()->json([
             "status" => 201,
             "message" => "data stored successfully!",
@@ -71,7 +75,7 @@ class OrderController extends Controller
             "menu_data" => $menu_data,
         ], 201);
     }
-        
+
 
     /**
      * Display the specified resource.
@@ -79,35 +83,40 @@ class OrderController extends Controller
     public function show(string $id)
     {
         $order = Order::findOrFail($id);
-        
-        $menuCounts = []; // Associative array to store menu counts
-        
-        foreach(json_decode($order->menu_ids) as $menu_id) {
-            $menu = Menu::find($menu_id);
-            
-            // If the menu is not in the array, add it with a count of 1
-            if (!isset($menuCounts[$menu->name])) {
-                $menuCounts[$menu->name] = [
-                    "menu_name" => $menu->name,
-                    "menu_price" => $menu->price,
-                    "amount" => 1
+
+        $menuNotes = json_decode($order->menu_notes);
+
+        $menuCounts = array_reduce(json_decode($order->menu_ids), function ($carry, $menuId) use ($order, &$menuNotes) {
+            $menu = Menu::find($menuId);
+
+            if (count($menuNotes) > 0) {
+                $currentNote = array_shift($menuNotes);
+            } else {
+                $currentNote = ""; // Assign an empty note if there are no more notes
+            }
+
+            if (!isset($carry[$menu->name])) {
+                $carry[$menu->name] = [
+                    'menu_name' => $menu->name,
+                    'menu_price' => $menu->price,
+                    'amount' => 1,
+                    'notes' => [$currentNote],
                 ];
             } else {
-                // If the menu is already in the array, increment the count
-                $menuCounts[$menu->name]['amount']++;
+                $carry[$menu->name]['amount']++;
+                $carry[$menu->name]['notes'][] = $currentNote;
             }
-        }
-        
-        $menu_data = array_values($menuCounts); // Convert associative array to indexed array
-        
+
+            return $carry;
+        }, []);
+
         return response()->json([
             "status" => 200,
             "message" => "data retrieved successfully!",
             "order" => $order,
-            "menus" => $menu_data
+            "menus" => $menuCounts,
         ], 200);
     }
-    
 
     /**
      * Update the specified resource in storage.
@@ -133,16 +142,16 @@ class OrderController extends Controller
         $order = Order::findOrFail($id);
 
         $menuCounts = []; // Associative array to store menu counts
-        
+
         $order->update([
             "table_number" => $validator["table_number"] ?? $order->table_number,
             "payed" => $validator["payed"] ?? $order->payed,
             "menu_ids" => $menu_ids,
         ]);
-        
-        foreach(json_decode($order->menu_ids) as $menu_id) {
+
+        foreach (json_decode($order->menu_ids) as $menu_id) {
             $menu = Menu::find($menu_id);
-            
+
             // If the menu is not in the array, add it with a count of 1
             if (!isset($menuCounts[$menu->name])) {
                 $menuCounts[$menu->name] = [
