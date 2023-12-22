f7e286993565ef7f1a4ed5d2abeebfca233e1bc5
Sendiko <sarangtawon897@gmail.com>
2023-12-22 16:02:30 +0700

    enc(add): dark bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
     </head>
-    <body class="antialiased">
+    <body class="antialiased bg-dark">
         <div class="d-flex p-5 justify-content-center align-content-center w-100 h-100 bg-dark">
             <img src="{{URL('/images/logo_and_text_white.png')}}"/>
         </div>
